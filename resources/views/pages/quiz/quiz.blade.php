e1440c7f9e2e9467778cf76d22701c27a7d7f957
RuhtraProjetos <111287545+RuhtraProjetos@users.noreply.github.com>
2022-10-26 11:54:51 -0400

    26.10.2022
    UPDATE
    - Modificações em Start e Quiz.

--- resources/views/pages/quiz/quiz.blade.php
+++ resources/views/pages/quiz/quiz.blade.php
@@ -9,26 +9,27 @@
                 <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Veja os diferentes testes para encontrar o perfil
                     ideal para você</p>
             </div>
-
             <div class="mt-10">
                 <dl class="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
                     <a class="group relative flex w-full justify-center rounded-md border border-transparent p-4 bg-white py-2 px-4 text-sm font-medium text-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        href="#">
+                        href="/quiz/entrepreneurProfileTest">
                         <div class="relative">
                             <dt>
                                 <div
                                     class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
                                     <svg class="h-6 w-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        viewBox="0 0 640 512" stroke-width="1.5" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                                            d="M48 0C21.5 0 0 21.5 0 48V464c0 26.5 21.5 48 48 48h96V432c0-26.5 21.5-48 48-48s48 21.5 48 48v80h89.9c-6.3-10.2-9.9-22.2-9.9-35.1c0-46.9 25.8-87.8 64-109.2V271.8 48c0-26.5-21.5-48-48-48H48zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm112-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM80 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V112zM272 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zM576 272c0-44.2-35.8-80-80-80s-80 35.8-80 80s35.8 80 80 80s80-35.8 80-80zM352 477.1c0 19.3 15.6 34.9 34.9 34.9H605.1c19.3 0 34.9-15.6 34.9-34.9c0-51.4-41.7-93.1-93.1-93.1H445.1c-51.4 0-93.1 41.7-93.1 93.1z" />
                                     </svg>
                                 </div>
-                                <p class="ml-16 text-lg font-medium leading-6 text-gray-900">Competitive exchange rates</p>
+                                <p class="ml-16 text-lg font-medium leading-6 text-gray-900">Teste de Perfil Empreendedor
+                                </p>
                             </dt>
-                            <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur
-                                adipisicing
-                                elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            <dd class="mt-2 ml-16 text-base text-gray-500">Empreender nos dias de hoje é uma atitude que
+                                requer muita determinação, propósito e visão. Essas são características que a maioria dos
+                                empreendedores apresenta, assim como o otimismo, persistência, capacidade de enxergar
+                                soluções e de liderar equipes, entre outras. Faça o teste e veja seu perfil empreendedor!
                             </dd>
                         </div>
                     </a>
